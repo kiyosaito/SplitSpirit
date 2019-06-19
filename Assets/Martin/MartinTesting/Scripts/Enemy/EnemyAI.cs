ff323395b96dd0245ab0c78b0d561cf79b43ffaa
kiyosaito <48036649+kiyosaito@users.noreply.github.com>
2019-06-20 00:22:03 +1000

    update

---
+++ Assets/Martin/MartinTesting/Scripts/Enemy/EnemyAI.cs
@@ -0,0 +1,23 @@
+﻿// =========================================================
+// Title: EnemyAI
+// Author(s): Martin Nguyen
+// Date: 19/06/2019
+// Details: Handles Waypoints for AI as well as a seek and patrol switch
+// Reference: 
+// =========================================================
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
