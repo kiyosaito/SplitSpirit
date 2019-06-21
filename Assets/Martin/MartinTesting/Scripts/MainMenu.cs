0788de5c5b1b13d8694533cf94f05a5aed5c2c5f
Jack <47583484+JackADV@users.noreply.github.com>
2019-06-21 22:24:42 +1000

    Update

---
+++ Assets/Martin/MartinTesting/Scripts/MainMenu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1); // Load the first scene
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit"); // Message to appear in the console during play mode
+        Application.Quit(); // closes the game
+    }
+
+
+}
+        
+
+
+
+
+
+
+
